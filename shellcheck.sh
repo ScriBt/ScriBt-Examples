1b2da494adfbe4754861e8547ffe84632e6f841b
Arvindraj <arvindultimate7352@gmail.com>
2017-05-11 23:50:33 +0530

    Commonize Travis (and GCloud) Warning Flags
    .. to be ignored for all scripts

--- shellcheck.sh
+++ shellcheck.sh
@@ -13,15 +13,18 @@ function derpcheck()
         echo -e "\n${SCS} ${CL_WYT}derpCheck says: $1: 404 No Derps${NONE}\n";
     else
         echo -e "\n${FLD} ${CL_WYT}derpCheck says: $1: Derped!${NONE}\n";
+        DERP="yes";
     fi
 } # derpcheck
 
 # Flags to be disabled for ROM.sh
-export SCOPTS_ROMsh="-e SC2155 -e SC2120 -e SC2119 -e SC2086 -e SC2046 -e SC2153 -e SC2016 -e SC2030 -e SC2031 -e SC2162 -e SC2164 -e SC1090";
+export SCOPTS_ROMsh="-e SC2155 -e SC2120 -e SC2119 -e SC2086 -e SC2046 -e SC2153 -e SC2016 -e SC2030 -e SC2031";
 # Flags to be disabled on ROM.rc
 export SCOPTS_ROMrc="-e SC2034 -e SC2153 -e SC2154";
 # Flags to be disabled on upScriBt.sh
 export SCOPTS_upScriBtsh="-e SC1083 -e SC2086";
+# Travis && GCloud flags to be disabled
+export TRAVOPTS="-e SC2162 -e SC2164 -e SC1090";
 
 NO=1;
 echo;
@@ -29,9 +32,11 @@ for file in ROM.sh ROM.rc upScriBt.sh; do
     # Run derpCheck
     echo -e "${EXE} ${CL_WYT}Running shellcheck on $file${NONE}";
     OPTS="-e $(eval echo -e "\${SCOPTS_${file/\./}}")";
-    shellcheck --shell=bash ${OPTS} "$file";
+    shellcheck --shell=bash ${OPTS} ${TRAVOPTS} "$file";
     derpcheck "$file";
     (( NO++ ));
 done
 
 unset SCOPTS_ROMsh SCOPTS_ROMrc SCOPTS_upScriBtsh OPTS NO file;
+
+if [[ "$DERP" == "yes" ]]; then exit 1; fi;
