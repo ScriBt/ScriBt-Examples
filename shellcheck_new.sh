c01ee2d427417e42876e7342db79e4160dd37518
Arvindraj <arvindultimate7352@gmail.com>
2017-06-11 15:49:56 +0530

    shellcheck_new: Modifications for new directory structure
    Signed-off-by: Arvindraj <arvindultimate7352@gmail.com>

--- shellcheck_new.sh
+++ shellcheck_new.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-# DISABLED shellcheck] FOR FILES OTHER THAN ROM.sh
-# WORK IN PROGRESS!
-
 # I want 'em colors!
 NONE="\033[0m";
 FLD="\033[1;31m[!]${NONE}";
@@ -12,7 +9,7 @@ WYT="\033[1;37m";
 
 function derpcheck()
 {
-    if [[ "$?" == "0" ]]; then
+    if shellcheck --shell=bash ${OPTS} ${COMMON} "$1"; then
         echo -e "\n${SCS} ${CL_WYT}derpCheck says: $1: 404 No Derps${NONE}\n";
     else
         echo -e "\n${FLD} ${CL_WYT}derpCheck says: $1: Derped!${NONE}\n";
@@ -22,24 +19,26 @@ function derpcheck()
 
 # Flags to be disabled for ROM.sh
 export SCOPTS_ROMsh="-e SC2154 -e SC2155 -e SC2120 -e SC2119 -e SC2086 -e SC2046 -e SC2153 -e SC2016 -e SC2030 -e SC2031";
-# Flags to be disabled on ROM.rc
-export SCOPTS_ROMrc="-e SC2034 -e SC2153 -e SC2154";
-# Flags to be disabled on upScriBt.sh
-export SCOPTS_upScriBtsh="-e SC2086";
+# Flags to be disabled for upScriBt.sh - NONE, YAY
+# export SCOPTS_upScriBtsh="-e SC2086";
+# Flags to be disabled for src files
+export SCOPTS_src="-e SC2034";
 # Common Flags
 export COMMON="-e SC2164 -e SC1090";
 
-NO=1;
-echo;
-for file in ROM.sh; do
-    # Run derpCheck
-    echo -e "${EXE} ${CL_WYT}Running shellcheck on $file${NONE}";
-    OPTS="-e $(eval echo -e "\${SCOPTS_${file/\./}}")";
-    shellcheck --shell=bash ${OPTS} ${COMMON} "$file";
+# Run derpCheck
+echo -e "\n${EXE} Running shellcheck on ${WYT}ROM.sh${NONE}";
+OPTS="${SCOPTS_ROMsh}";
+derpcheck "ROM.sh";
+
+for file in $(find ./src -type f -path ./src/utils -prune -o -name '*.rc'); do
+    # Run derpCheck on src files
+    echo -e "\n${EXE} Running shellcheck on ${WYT}${file}${NONE}";
+    OPTS="${SCOPTS_src}";
     derpcheck "$file";
     (( NO++ ));
 done
 
-unset SCOPTS_ROMsh SCOPTS_ROMrc SCOPTS_upScriBtsh OPTS NO file;
+unset SCOPTS_ROMsh SCOPTS_src SCOPTS_upScriBtsh OPTS file;
 
-if [[ "$DERP" == "yes" ]]; then exit 1; fi;
+if [[ "$DERP" == "yes" ]]; then exit 1; else echo -e "\n${SCS} ScriBt is free from Derps\n"; exit 0; fi;
